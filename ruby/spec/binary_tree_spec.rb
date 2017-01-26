220e1b5d02cf100b9a6fa21c40053cccb3d26e89
Greggory Rothmeier <greggroth@gmail.com>
2017-01-26 10:13:28 -0800

    Add test case showing keys rather than indicies
    The library uses the `#[]` method on the vector passed to `call`, so any
    valid argument as the `feature_idx` to get a value from that vector is
    acceptable.  In this case, I've added an example where a hash with keys
    are used rather than an array with indicies.

--- ruby/spec/binary_tree_spec.rb
+++ ruby/spec/binary_tree_spec.rb
@@ -9,4 +9,15 @@ describe TreeThinking::BinaryTree do
       expect(tree.call([1, 1])).to eq(0.8)
     end
   end
+
+  context 'with a tree with key indicies' do
+    subject(:tree) { TreeThinking::BinaryTreeFactory.from_yaml('./spec/fixtures/key_tree.yaml') }
+
+    describe '#call' do
+      it 'returns a probablity from an answer vector' do
+        expect(tree.call({'likes_ice_cream' => 1, 'likes_chocolate' => 0})).to eq(0.1)
+        expect(tree.call({'likes_ice_cream' => 1, 'likes_chocolate' => 1})).to eq(0.8)
+      end
+    end
+  end
 end
